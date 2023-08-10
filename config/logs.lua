ed0026555dfcba4d1197d4c6fcc882c2ea0fdf50
DarkExEToR17 <115365072+DarkExEToR17@users.noreply.github.com>
2023-08-10 20:12:11 +0200

    Create logs.lua

---
+++ config/logs.lua
@@ -0,0 +1,25 @@
+_1 = "[+] Loading Zerarchi Hub"
+_2 = "[=] Finding Admin"
+_3 = "[=] Checking BlackList"
+_4 = "[+] Bypassing"
+_5 = "[+] Loading Gui"
+_6 = "[=] Welcome To ZerarchiHub"
+
+
+
+
+
+
+
+
+_err466 = "Error Code : 466 Message : You are Blacklisted"
+_err988 = "Error Code : 988 Message : Develloper Shutdowned the Script Or The script is Patched"
+
+
+
+
+
+
+
+_noadm = "[+] No admin Detected"
+_adm = "[+] Admin Detected"
