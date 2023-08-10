b4b96edbe3c76e8e89f96e0917f393c478b14f72
DarkExEToR17 <115365072+DarkExEToR17@users.noreply.github.com>
2023-08-10 19:50:17 +0200

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,5 +1,24 @@
 _ad1 = "leroidu45"
 _ad2 = "CoolSupX_0"
+_ad3 = "nil"
+_ad4 = "nil"
+_ad5 = "nil"
+
+_astra = "SPDM_Astra | flipper_zero | Dev,Scripter,Gui Maker"
+_heclom = "heclom  |  ?????  |  Support,Helper,Gui Helper,Friend,Admin"
+_sigma = "sigma  |  giga_Sigma200  |  Friend,Admin,Helper"
 
 
 _scmsg = "Succes"
+
+_blc = "nil"
+_blc1 = "nil"
+_blc2 = "nil"
+_blc3 = "nil"
+_blc4 = "nil"
+_blc5 = "nil"
+_blc6 = "nil"
+_blc7 = "nil"
+_blc8 = "nil"
+_blc9 = "nil"
+_blc01 = "nil"
